1d8028f7e9b469eb24f42314b10fac23446085d8
faheem6828 <faheem20198@gmail.com>
2026-02-18 12:54:26 +0600

    new works added

---
+++ Milestone 03/Module 14/complexConditions.js
@@ -0,0 +1,9 @@
+let salary = 21000;
+let height = 6;
+let isBcs = true;
+
+if ((salary > 20000 && isBcs == true) || height > 60) {
+  console.log("You are selected");
+} else {
+  console.log("You are not Selected");
+}
