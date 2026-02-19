69403b4756cad99d8d93246671e68c0440a36cde
faheem6828 <faheem20198@gmail.com>
2026-02-19 23:42:52 +0600

    new module started, Array

---
+++ Milestone 03/Module 14_5/array.js
@@ -0,0 +1,10 @@
+/**_______________________________________________________________________________________
+ * ARRAY: it enables storing a collection of multiple items under a single variable name
+ * _______________________________________________________________________________________
+ */
+
+
+const fruits =['apple', 'mango', 'orange'];
+fruits[0] = 'Lichu';
+console.log(fruits)
+
